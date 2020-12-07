2517738ac7d96a1aae7d7d8c80af03239bfa4ede
andersonjackc <60368246+andersonjackc@users.noreply.github.com>
2020-12-07 11:10:12 -0500

    Added 2 Targets
    Added Collision to 2 Targets in SampleScene

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TestScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        Debug.Log(collision);
     }
 }
