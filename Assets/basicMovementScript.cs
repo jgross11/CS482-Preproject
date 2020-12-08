ec09fed734fe2d2a3f370721826a4672d7df07da
jgross11 <jgross11@ycp.edu>
2020-12-07 19:07:08 -0500

    Created basicMovementScript
    Describes automatic movement in one direction

---
+++ Assets/basicMovementScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicMovementScript : MonoBehaviour
+{
+
+    // movement speed multiplier, keep 1 < movementSpeed < 1...
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(Time.deltaTime*movementSpeed, 0, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        Destroy(this.gameObject);
+    }
+}
