b09bfe9d44e3b21103af29c8004f6629891fb9b2
jgross11 <jgross11@ycp.edu>
2020-12-09 10:21:31 -0500

    Added scene transition script
    Loads a scene by its name and preserves indicated gameobjects

---
+++ Assets/scripts/sceneTransitioner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneTransitioner : MonoBehaviour
+{
+    // name of scene to transfer to
+    public string transferSceneName;
+
+    // array of game objects to bring along to the next scene
+    public GameObject[] objectsToPreseve;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        // mark each preservable game object as such
+        if(objectsToPreseve.Length > 0){
+            foreach(GameObject obj in objectsToPreseve)
+            {
+                DontDestroyOnLoad(obj);
+            }
+        }
+
+        // load the appropriate scene
+        if(transferSceneName != null){
+            Debug.Log("transitioning to scene " + transferSceneName);
+            SceneManager.LoadScene(transferSceneName);
+        }
+    }
+}
