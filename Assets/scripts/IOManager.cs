9167bdca8b5cdccf0e392edb2a64b870343eedd1
jgross11 <jgross11@ycp.edu>
2020-12-10 08:51:54 -0500

    Created IOManager
    will manage loading and saving of player information

---
+++ Assets/scripts/IOManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IOManager
+{
+    // save player information to local file
+    public static bool saveToLocalFile(SaveObject so){
+        Debug.LogError("Not yet implemented!");
+    }
+
+    // load player information from local file
+    public static SaveObject loadFromLocalFile(string playerName){
+        Debug.LogError("Not yet implemented!");
+    }
+
+    // save player information to playerpreferences
+    public static bool saveToPlayerPreferences(SaveObject so){
+        Debug.LogError("Not yet implemented!");
+    }
+
+    // load player information from playerpreferences
+    public static SaveObject loadFromPlayerPreferences(string playerName){
+        Debug.LogError("Not yet implemented!");
+    }
+
+    // save player information to binary file
+    public static bool saveToBinary(SaveObject so){
+        Debug.LogError("Not yet implemented!");
+    }
+
+    // load player information from binary file
+    public static SaveObject loadFromBinary(string playerName){
+        Debug.LogError("Not yet implemented!");
+    }
+}
