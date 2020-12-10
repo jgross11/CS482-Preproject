2e0331fba2821858ef3b8fa84ce5d219e0825de0
jgross11 <jgross11@ycp.edu>
2020-12-10 08:54:09 -0500

    Update IOManager.cs
    placeholder returns

--- Assets/scripts/IOManager.cs
+++ Assets/scripts/IOManager.cs
@@ -7,30 +7,36 @@ public class IOManager
     // save player information to local file
     public static bool saveToLocalFile(SaveObject so){
         Debug.LogError("Not yet implemented!");
+        return false;
     }
 
     // load player information from local file
     public static SaveObject loadFromLocalFile(string playerName){
         Debug.LogError("Not yet implemented!");
+        return null;
     }
 
     // save player information to playerpreferences
     public static bool saveToPlayerPreferences(SaveObject so){
         Debug.LogError("Not yet implemented!");
+        return false;
     }
 
     // load player information from playerpreferences
     public static SaveObject loadFromPlayerPreferences(string playerName){
         Debug.LogError("Not yet implemented!");
+        return null;
     }
 
     // save player information to binary file
     public static bool saveToBinary(SaveObject so){
         Debug.LogError("Not yet implemented!");
+        return false;
     }
 
     // load player information from binary file
     public static SaveObject loadFromBinary(string playerName){
         Debug.LogError("Not yet implemented!");
+        return null;
     }
 }
