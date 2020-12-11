53d94919b2bb0df04801ab8944987963440e0062
jgross11 <jgross11@ycp.edu>
2020-12-10 22:12:04 -0500

    Created towerMenuOption
    Created script to handle setting of selected employee tower within menu

---
+++ Assets/towerMenuOption.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// handles setting of selected employee
+public class towerMenuOption : MonoBehaviour
+{
+    // the menu script whose selected employee will be changing
+    public playMenuHandler menuScript;
+
+    // the index in playMenuHandler.availableEmployees
+    // whose employee is displayed in this menu tile
+    public int employeeIndex;
+
+    void OnMouseDown(){
+        menuScript.SetSelectedEmployee(employeeIndex);
+    }
+}
