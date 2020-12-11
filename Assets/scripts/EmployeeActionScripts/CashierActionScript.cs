3f3991b706c9959ce8bfefab22b42fd520bbdf29
jgross11 <jgross11@ycp.edu>
2020-12-11 14:24:22 -0500

    Created CashierActionScript.cs
    Created action script for cashier tower
    Cashiers generate coins whenever possible

---
+++ Assets/scripts/EmployeeActionScripts/CashierActionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// cashier action logic
+public class CashierActionScript : ActionScript
+{
+
+    public GameObject coin;
+    // cashiers can generate a coin as soon as possible,
+    // so this method just returns true
+    public override bool CanAct()
+    {
+        return true;
+    }
+
+    public override void Act(int attack)
+    {
+        Instantiate(coin, transform.position, transform.rotation).GetComponent<CashierCoinScript>().setValue(attack/3 + 1);
+    }
+}
