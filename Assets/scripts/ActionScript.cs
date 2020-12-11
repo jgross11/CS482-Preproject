1728596f10955511d1dcd26708f6fac1fe54d251
jgross11 <jgross11@ycp.edu>
2020-12-11 14:23:43 -0500

    Created ActionScript.cs
    Created abstract class that determines if a tower's action can occur as well as execution of the action itself

---
+++ Assets/scripts/ActionScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// abstract class representing an action taken by a tower (and zombie?)
+public abstract class ActionScript : MonoBehaviour
+{
+    // must return true if the tower can act, false otherwise
+    public abstract bool CanAct();
+
+    // executes the actual action the tower does
+    // the results of this action are dependent on 
+    // one of the employee's stats
+    public abstract void Act(int scalingStat);
+}
