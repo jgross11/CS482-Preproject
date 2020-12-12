6a4b93e8bef351c6eb4b74eeed6127ddf1dd0013
andersonjackc <60368246+andersonjackc@users.noreply.github.com>
2020-12-11 19:31:45 -0500

    Added Can, Added VendMach Person, and Implemented their Action
    The Can gets destroyed on collision but no damage is done to the Zombie

---
+++ Assets/scripts/EmployeeActionScripts/CanScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanScript : MonoBehaviour
+{
+    private int value;
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        direction = new Vector3(1, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * 0.003f;
+
+
+    }
+
+    public void setValue(int val)
+    {
+        value = val;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/scripts/EmployeeActionScripts/VendingMachineActionScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VendingMachineActionScript : ActionScript
+{
+    public GameObject can;
+    // cashiers can generate a coin as soon as possible,
+    // so this method just returns true
+    public override bool CanAct()
+    {
+        return true;
+    }
+
+    public override void Act(int attack)
+    {
+        Instantiate(can, transform.position, transform.rotation).GetComponent<CanScript>().setValue(attack / 3 + 1);
+    }
+}
