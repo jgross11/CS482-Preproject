904fa119a8e92d35fe0f15a8c6e6a3bc0136ae53
jgross11 <jgross11@ycp.edu>
2020-12-14 17:19:16 -0500

    pharmacist comments

--- Assets/scripts/EmployeeActionScripts/PharmacistActionScript.cs
+++ Assets/scripts/EmployeeActionScripts/PharmacistActionScript.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 
 public class PharmacistActionScript : ActionScript
 {
-
+    // tower to target when healing
     public GameObject target;
+
+    // prefab for pill to create
     public GameObject pillPrefab;
+
+    // indicates whether or not this tower is currently selecting a target
     public bool isSelecting = false;
+
+    // the employee tower's collider
     public PolygonCollider2D parentCollider;
+
+    // the employee tower's sprite
     public SpriteRenderer parentSpriteRenderer;
+
+    // the target tower's sprite
     public SpriteRenderer targetSpriteRenderer;
+
+    // number of pills created in total
     public int pillsOut;
+
+    // maximum amount of pills able to be out at any given time
     public int maxAmountOfPills;
     
     void Start()
@@ -20,67 +34,108 @@ public class PharmacistActionScript : ActionScript
         // reset isSelecting to false after a short period of time to circumvent
         // bug wherein placing this tower automatically selects it
         StartCoroutine(EnableSelection());
+
+        // at start, no pills have been created
         pillsOut = 0;
+
+        // max amount of pills = pill lifetime / tower firing speed
         maxAmountOfPills = (int) (pillPrefab.GetComponent<PharmacistPillScript>().lifeTime / parentCollider.GetComponent<Employee>().timeBetweenActions);
     }
 
+    // reset isSelecting after a short period of time
     private IEnumerator EnableSelection(){
         yield return new WaitForSeconds(0.001f);
         isSelecting = false;
+
+        // update tower / target sprite colors
         UpdateColors(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if the left mouse button is clicked
         if(Input.GetMouseButtonDown(0)){
+
+            // get world space from mouse position on screen and collect information regarding possible collision
             RaycastHit2D rayHit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+
+            // if tower is not selecting and itself was clicked
             if (!isSelecting && rayHit.collider == parentCollider){
+                
+                // tower is now selecting
                 isSelecting = true;
+
+                // indicate sprite is selected
                 UpdateColors(true);
-                Debug.Log("now selecting target");
+
+            // otherwise if tower is selecting and nothing or current target is selected
             } else if(isSelecting && rayHit.collider == null || (target != null && rayHit.collider == target.GetComponent<Collider>())){
+                
+                // cancel selecting
                 isSelecting = false;
-                Debug.Log("canceling selection 1");
+                
+                // indicate sprite is not selected
                 UpdateColors(false);
+
+            // otherwise if tower is selecting and an employee was selected
             } else if(rayHit.collider != null && rayHit.collider.tag == "employee" && isSelecting){
+                
+                // if another tower was already selected 
                 if(target != null){
+
+                    // undo graphical representation of selection on old tower
                     targetSpriteRenderer.color = Color.white;
                 }
+
+                // target becomes the newly selected tower
                 target = rayHit.collider.gameObject;
+
+                // get new target sprite
                 targetSpriteRenderer = target.GetComponent<SpriteRenderer>();
-                Debug.Log("selected target");
+
+                // stop selecting
                 isSelecting = false;
+
+                // get target employee script
                 Employee targetScript = target.GetComponent<Employee>();
-                Debug.Log(targetScript.type);
+
+                // if the target is a pharmacist
                 if(targetScript.type == "pharmacist"){
-                    target.GetComponentInChildren<PharmacistActionScript>().SetSelected(false);
+                    // wait a short amount of time and reset target's selection boolean
+                    // this prevents double selection bug
+                    StartCoroutine(target.GetComponentInChildren<PharmacistActionScript>().EnableSelection());
                 }
+
+                // update this and target's sprites to indicate no more selecting
                 UpdateColors(false);
             }
         }
-
-
-        // TODO make this more apparent
-        if(isSelecting){
-            
-        } else{
-
-        }
-
     }
 
+    // updates the sprite colors of both this tower and its target
+    // isSelected: true to indicate this tower and its target (if applicable) are selected
+    // false otherwise
     private void UpdateColors(bool isSelected){
+        // if towers are selected
         if(isSelected){
+
+            // if target exists and is not self, indicate both towers are selected
             if(target != null && target.gameObject != parentSpriteRenderer.gameObject){
                 targetSpriteRenderer.color = Color.red;
                 parentSpriteRenderer.color = Color.cyan;
+
+            // otherwise if target is self, indicate both selections simultaneously
             } else if(target.gameObject == parentSpriteRenderer.gameObject){
                 parentSpriteRenderer.color = Color.magenta;
             }
+            
+            // otherwise no target exists, just indicate this tower is selected
              else{
                 parentSpriteRenderer.color = Color.cyan;
              }
+
+        // otherwise, reset this and target tower's sprite to default
         } else{
             parentSpriteRenderer.color = Color.white;
             if(target != null){
@@ -89,32 +144,44 @@ public class PharmacistActionScript : ActionScript
         }
     }
 
+    // returns this tower's target
     public GameObject GetTarget(){
         return target;
     }
 
+    // pharmacists can act ASAP, just return true
     public override bool CanAct(){
         return true;
     }
 
+    // pharmacist action: the action of a pharmacist is to produce a pill that heals
+    // create pill in front of this tower with this tower's attack as its healing power
     public override void Act(int attack)
     {
+        // create instance of pill here, obtain reference to its script
         PharmacistPillScript pillScript = Instantiate(pillPrefab, transform.position, transform.rotation).GetComponent<PharmacistPillScript>();
+
+        // set healing power relative to this tower's attack
+        // TODO balancing
         pillScript.SetValue(attack/3 + 1);
+
+        // set pill's initial target to be the currently selected tower 
         pillScript.SetTarget(target);
+
+        // attach reference to this script to use when finding new target
         pillScript.SetParentScript(this);
 
+        // generate correct position for pill to move to
+        // spreads in a semi- half circle around tower in a cyclic fasion
         // do not worry about this math, it is totally legit
         float xPos = 1.5f * (float) Math.Sin(-(Math.PI / 4.0) + (((pillsOut % maxAmountOfPills) + 2) * Math.PI / (float) (maxAmountOfPills+1)));
         float yPos = 1.5f * (float) Math.Cos(-(Math.PI / 4.0) + (((pillsOut % maxAmountOfPills) + 2) * Math.PI / (float) (maxAmountOfPills+1)));
 
+        // increment number of pills that have been created
         pillsOut++;
 
-        pillScript.SetDirection(new Vector3(transform.position.x + xPos, transform.position.y + yPos, 0));
+        // set pill's target direction to the generated x, y pos
+        pillScript.SetDesiredPosition(new Vector3(transform.position.x + xPos, transform.position.y + yPos, 0));
 
-    }
-
-    public void SetSelected(bool state){
-        StartCoroutine(EnableSelection());
     }
 }
--- Assets/scripts/EmployeeActionScripts/PharmacistPillScript.cs
+++ Assets/scripts/EmployeeActionScripts/PharmacistPillScript.cs
@@ -5,68 +5,118 @@ using UnityEngine;
 public class PharmacistPillScript : MonoBehaviour
 {
     
+    // healing value of this pill
     public int value;
+
+    // the target tower to move towards when active
     public GameObject target;
+
+    // indicates whether this pill is moving towards target or not
     public bool isActive;
+
+    // position to move towards when created
     private Vector3 randDirection;
+
+    // time to move from tower origin to randDirection
     public float maxMoveTime = 1.0f;
+
+    // amount of time this pill has been alive for
     private float currentLifeTime = 0.0f;
+
+    // amount of time to wait until destroying this pill
     public float lifeTime = 7.0f;
+
+    // speed at which pill moves towards target when active
     public float moveSpeed = 15.0f;
+
+    // the tower which created this pill
     public PharmacistActionScript parentScript;
     
     // Start is called before the first frame update
     void Start()
     {
-        // randDirection = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
+        
     }
 
-    public void SetDirection(Vector3 dir){
+    // set the desired position of this pill
+    public void SetDesiredPosition(Vector3 dir){
         randDirection = dir;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // add to pill's current lifetime
         currentLifeTime += Time.deltaTime;
+
+        // if pill is active
         if(isActive){
+            
+            // move towards target and rotate for neat effect
             transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
             transform.Rotate(Vector3.forward);
         }
+
+        // otherwise if pill still needs to move towards desired position
         else if(currentLifeTime < maxMoveTime){
+            
+            // move pill towards desired position
             transform.position = Vector2.MoveTowards(transform.position, randDirection, moveSpeed * Time.deltaTime);
         }
+
+        // otherwise if pill has overstayed its welcome
         else if(currentLifeTime >= lifeTime){
+
+            // destroy pill
             Destroy(this.gameObject);
         }
     }
 
+    // set healing value 
     public void SetValue(int val){
         value = val;
     }
 
+    // set target tower
     public void SetTarget(GameObject newTarget){
         target = newTarget;
     }
 
+    // set parent tower's script reference 
     public void SetParentScript(PharmacistActionScript ps){
         parentScript = ps;
     }
 
+    // when the pill is clicked on 
     void OnMouseDown(){
+
+        // get tower's current target
         target = parentScript.GetTarget();
+
+        // if target exists
         if(target != null){
+
+            // indicate pill should move towards tower
             isActive = true;
         }
     }
 
+    // when pill collides with something
     void OnTriggerEnter2D(Collider2D col){
+        
+        // if the pill collides with its target
         if(isActive && target != null && col == target.GetComponent<Collider2D>()){
+            
+            // heal target by this pill's amount
             target.GetComponent<Employee>().Heal(value);
+
+            // destroy pill
             Destroy(this.gameObject);
         }
     }
 
+    // if pill happens to already have been colliding with target while active
+    // same thing as OnTriggerEnter2D
     void OnTriggerStay2D(Collider2D col){
         if(isActive && target != null && col == target.GetComponent<Collider2D>()){
             target.GetComponent<Employee>().Heal(value);
