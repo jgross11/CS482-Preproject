36df359c25ec034c52065e25a4ba46a301a86613
jgross11 <jgross11@ycp.edu>
2020-12-14 20:05:46 -0500

    Update playMenuHandler.cs
    Improved menu logic
    - unaffordable towers now gray, recomputed with coin count change
    - selected tower is deselected upon clicking unaffordable tower
    - can place multiple instances of selected tower so long as player has enough coins

--- Assets/scripts/Playmode Scripts/playMenuHandler.cs
+++ Assets/scripts/Playmode Scripts/playMenuHandler.cs
@@ -21,8 +21,8 @@ public class playMenuHandler : MonoBehaviour
     // the employee tower the player is considering creating
     public GameObject selectedEmployee;
     
-    // TODO script containing employee tower information
-    // public EMPLOYEE SCRIPT NAME HERE employeeScript; 
+    // selected employee script
+    public Employee employeeScript; 
 
     // number of coins / happiness (short term currency) gathered thus far
     private int numberOfCoins = 0;
@@ -44,15 +44,39 @@ public class playMenuHandler : MonoBehaviour
             // set menu sprite to this employee's sprite
             slot.GetComponent<SpriteRenderer>().sprite = go.GetComponent<SpriteRenderer>().sprite;
         }
+
+        // no tower is initially selected
+        selectedSlotIndex = -1;
+
+        // compute initial affordability
+        RefreshAffordableTowers();
     }
     
+    /*
+        Completely deselects the currently selected tower option in the menu
+    */
+    public void Deselect(){
+
+        // reset menu tower sprite color
+        menuSlots[selectedSlotIndex].GetComponent<SpriteRenderer>().color = CanAffordTower(employeeScript) ? Color.white : Color.grey;
+
+        // wipe selected tower index
+        selectedSlotIndex = -1;
+
+        // wipe selected employee gameobject
+        selectedEmployee = null;
+
+        // wipe selected employee script
+        employeeScript = null;
+    }
+
     /*
         Given the index of the employee in availableEmployees,
         handle menu sprite state changes
     */
     public void SetSelectedEmployee(int index){
-        // if index is valid (a valid tower preview has been clicked)
-        if(index > -1 && index < availableEmployees.Length){
+        // if index is valid (a valid tower preview has been clicked) and the targeted tower is affordable
+        if(index > -1 && index < availableEmployees.Length && CanAffordTower(availableEmployees[index].GetComponent<Employee>())){
 
             // if a sprite has already been selected
             if(selectedEmployee != null){
@@ -69,7 +93,7 @@ public class playMenuHandler : MonoBehaviour
             menuSlots[index].GetComponent<SpriteRenderer>().color = Color.gray;
 
             // update reference to employee information script to draw stats
-            // employeeScript = selectedEmployee.GetComponent<EMPLOYEE SCRIPT NAME HERE>();
+            employeeScript = selectedEmployee.GetComponent<Employee>();
         
         // otherwise, an invalid index was passed (invalid click, etc.)
         } else{
@@ -100,25 +124,45 @@ public class playMenuHandler : MonoBehaviour
     // decrement selected employee cost from current
     // number of coins and return the referenced employee prefab
     public GameObject CreateSelectedEmployee(){
-        // numberOfCoins -= employeeScript.cost;
-
+        AddCoins(-employeeScript.cost);
         return selectedEmployee;
     }
 
-
-    // determines if the selected tower can be 'bought'
-    // returns true if the tower can be bought, false otherwise 
+    // determines if the currently selected tower can be bought
+    // returns true if the tower can be bought, false otherwise
     public bool CanAffordTower(){
-        return true;
-        // return numberOfCoins >= employeeScript.cost;
+        return numberOfCoins >= employeeScript.cost;
+    }
+
+    // determines if the given Employee can be bought
+    // returns true if the tower can be bought, false otherwise
+    public bool CanAffordTower(Employee es){
+        return numberOfCoins >= es.cost;
+    }
+
+    /*
+        Iterates through list of available employees and
+        changes their menu preview colors appropriately
+    */
+    public void RefreshAffordableTowers(){
+        for(int i = 0; i < availableEmployees.Length; i++)
+        {
+            if(i == selectedSlotIndex){
+                continue;
+            }
+            // a mouthful
+            menuSlots[i].GetComponent<SpriteRenderer>().color = CanAffordTower(availableEmployees[i].GetComponent<Employee>()) ? Color.white : Color.grey;
+        }
     }
 
     // adds the given number of coins (short term currency)
-    // to the total amount
+    // to the total amount and recomputes affordable towers
     public void AddCoins(int number){
         numberOfCoins += number;
+        RefreshAffordableTowers();
     }
 
+    // get number of coins
     public int GetCoins()
     {
         return numberOfCoins;
