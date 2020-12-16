d7414e449d3b012be0644bf18574fe6db71bd4d2
jgross11 <jgross11@ycp.edu>
2020-12-15 21:22:44 -0500

    Create ProduceCarrotScript
    created script that handles creation, movement, destruction of produce carrot projectile

---
+++ Assets/scripts/EmployeeActionScripts/ProduceCarrotScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProduceCarrotScript : MonoBehaviour
+{
+    // damage value 
+    public int value;
+
+    // movement speed 
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // fix rotation issue
+        transform.Rotate(0, 0, 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move carrot forward relative to its move speed
+        transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+        transform.Rotate(0, 0, -1);
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        
+        // if collided with zombie
+        if(col.tag == "zombie"){
+            // TODO implement zombie please
+            
+            // damage the zombie by this carrot's damage value
+            // col.GetComponent<Zombie>().damage(value);
+
+            // destroy the carrot
+            Destroy(this.gameObject);
+        }
+    }
+
+    // set this carrot's damage value
+    public void SetValue(int val){
+        value = val;
+    }
+}
