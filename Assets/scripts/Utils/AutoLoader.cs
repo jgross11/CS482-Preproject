cf6aa19ae442ca4b43e7d2388db2598a66b1427f
jgross11 <jgross11@ycp.edu>
2020-12-18 18:53:30 -0500

    Create AutoLoader.cs
    Created script to bypass main menu if a save file already exists

---
+++ Assets/scripts/Utils/AutoLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        IOManager.LoadFromPlayerPreferences();
+        if(IOManager.saveDataExists){
+            SceneTransitioner.Transition("Action Menu");
+        }
+    }
+}
