087c77b0c52fd6579904b967d607e7248b0904bf
jgross11 <jgross11@ycp.edu>
2020-12-18 09:08:04 -0500

    Update SaveObject.cs
    Modified save state script to allow easy access / modifying of members

--- Assets/scripts/Utils/SaveObject.cs
+++ Assets/scripts/Utils/SaveObject.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaveObject : MonoBehaviour
+public static class SaveObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static int numHappiness;
 }
