dabc8ab7d357020db67720e29c65aab4de1d4e42
jgross11 <jgross11@ycp.edu>
2020-12-18 18:52:27 -0500

    Update SaveObject.cs
    Added fields for highest swarm and campaign waves

--- Assets/scripts/Utils/SaveObject.cs
+++ Assets/scripts/Utils/SaveObject.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public static class SaveObject
 {
     public static int numHappiness;
+    public const string HAPPINESS = "happiness";
+    public static int maxSwarmWave;
+    public const string MAX_SWARM_WAVE = "maxswarmwavenumber";
+    public static int maxCampaignWave;
+    public const string MAX_CAMPAIGN_WAVE = "maxcampaignwavenumber";
 }
