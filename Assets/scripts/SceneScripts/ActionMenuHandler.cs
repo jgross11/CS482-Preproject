f3e74ca064c0385624677d8157f680cf8630251b
jgross11 <jgross11@ycp.edu>
2020-12-18 18:54:27 -0500

    Create ActionMenuHandler
    Created script to handle un/locking of additional modes, displaying user save data graphically

---
+++ Assets/scripts/SceneScripts/ActionMenuHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionMenuHandler : MonoBehaviour
+{
+    
+    public Button campaignButton;
+    public Button researchButton;
+    public Button swarmButton;
+    public Text userInfoText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swarmButton.interactable = SaveObject.maxCampaignWave > 20;
+        researchButton.interactable = SaveObject.maxCampaignWave > 40;
+        userInfoText.text = "\nHappiness currency count: " + SaveObject.numHappiness;
+        userInfoText.text += "\nHighest campaign wave: " + SaveObject.maxCampaignWave;
+        userInfoText.text += "\nHighest swarm wave: " + SaveObject.maxSwarmWave;
+    }
+}
