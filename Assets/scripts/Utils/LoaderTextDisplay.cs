671088cbd023d0532cc4f4a05ca629280c4ea5d0
jgross11 <jgross11@ycp.edu>
2020-12-18 09:07:26 -0500

    Create LoaderTextDisplay.cs
    simple script which demonstrates accessing and modifying save state fields

---
+++ Assets/scripts/Utils/LoaderTextDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoaderTextDisplay : MonoBehaviour
+{
+    public Text display;
+
+    void Update()
+    {
+        display.text = "Happiness: " + SaveObject.numHappiness;
+    }
+
+    public void IncrementHappiness(){
+        SaveObject.numHappiness++;
+    }
+
+    public void DecrementHappiness(){
+        SaveObject.numHappiness--;
+    }
+}
