16a18910b45bbaa68b5d578b45b85bde114679b6
jgross11 <jgross11@ycp.edu>
2020-12-19 09:34:27 -0500

    Create WipeAllSaveData.cs
    Created script that wipes PlayerPrefs at the start of the scene

---
+++ Assets/scripts/Utils/WipeAllSaveData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WipeAllSaveData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+        Debug.Log("Player prefs wiped!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
