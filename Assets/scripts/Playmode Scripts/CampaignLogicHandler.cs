a4af02bc03079d1358e1cf66816cd88af09d59c5
jgross11 <jgross11@ycp.edu>
2020-12-19 09:08:45 -0500

    Update CampaignLogicHandler.cs
    Added logic to transition to next wave upon no zombies alive and none needing to be spawned

--- Assets/scripts/Playmode Scripts/CampaignLogicHandler.cs
+++ Assets/scripts/Playmode Scripts/CampaignLogicHandler.cs
@@ -7,34 +7,56 @@ public class CampaignLogicHandler : MonoBehaviour
     // used to reveal only the appropriate towers for player to use
     public playMenuHandler menuScript;
 
-    // used to determine... something TODO make number of alive zombies tracked here
-    public ZombieSpawner spawnerScript;
+    // the prefab for a blank board
+    public GameObject clearBoardPrefab;
 
-    // number of zombies currently alive
-    public int numAliveZombies;
+    // the instance of the board to wipe
+    public GameObject boardInstance;
 
-    // whether or not the zombie spawner is done spawning
-    // TODO transfer last two fields to zombie spawner script
-    public bool doneSpawning;
-    
+    // the position at which to place a new board
+    public Vector3 initialBoardPos;
+
+    // coin spawn time reset upon loading of new wave
+    public coinSpawner coinScript;
+
+    // used to obtain next wave contents
+    public CampaignZombieSpawner spawnerScript;
 
     void Start(){
         // TODO determine what the scaling formula for tower unlocks in campaign mode is
         // allow only the appropriate employees to be used, more to be added as campaign progresses
         menuScript.availableEmployees = TrimAvailableEmployees(SaveObject.maxCampaignWave / 5 + 1);
 
-        // zombies still need to spawn
-        doneSpawning = false;
+        // obtain original board position to reference when placing new board upon wave ending
+        initialBoardPos = boardInstance.transform.position;
     }
 
     void Update(){
-        // if no more zombies are alive and none need to be spawned, wave is complete
-        if(doneSpawning && numAliveZombies == 0){
-            Debug.Log("Completed wave!");
-        }
+        
     }
 
-    // newIndexAmount - the upper index whose employee will be displayed. All higher-indices employees are locked
+    // handles all logic necessary to mark the end of the current wave
+    public void EndCurrentWave(){
+
+        // increment max campaign wave in save file
+        PlayerPrefs.SetInt(SaveObject.MAX_CAMPAIGN_WAVE, ++SaveObject.maxCampaignWave);
+        
+        // reset board to blank
+        Destroy(boardInstance);
+        boardInstance = Instantiate(clearBoardPrefab, initialBoardPos, Quaternion.identity);
+
+        // reset coin spawn timer
+        coinScript.currentTime = 0;
+
+        // set number of coins to start with for next wave
+        // TODO determine proper scaling function for coin count
+        menuScript.SetCoins(10 + SaveObject.maxCampaignWave);
+
+        // start next wave
+        spawnerScript.LoadNextWave();
+    }
+
+    // newIndexAmount - the upper index whose employee will be displayed. All higher-indice employees are locked
     private GameObject[] TrimAvailableEmployees(int newIndexAmount){
 
         // create subarray
