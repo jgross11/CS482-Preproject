c9f876022e751f79c6eab6beb06cb2ea0c91e6bd
jgross11 <jgross11@ycp.edu>
2020-12-19 09:09:40 -0500

    Create CampaignZombieDestroyListener.cs
    Created script that is attached to campaign zombies upon their creation to keep track of the number of zombies still alive in the current wave

---
+++ Assets/scripts/Playmode Scripts/CampaignZombieDestroyListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// this script is attached upon the creation of a zombie in campaign mode
+// in order to accurately track the number of zombies still alive in that wave
+public class CampaignZombieDestroyListener : MonoBehaviour
+{
+    // spawner whose numAliveZombies will be decremented upon this being destroyed
+    private CampaignZombieSpawner spawnerScript;
+
+    void OnDestroy(){
+        
+        // if reference is valid
+        if(spawnerScript != null){
+            
+            // decrement the number of alive zombies from this spawner
+            spawnerScript.numAliveZombies--;
+        }       
+    }
+
+    public void SetSpawner(CampaignZombieSpawner s){
+        spawnerScript = s;
+    }
+}
