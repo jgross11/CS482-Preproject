879181e8387a8bcdf59ee52fa12c92517be1fe5c
jgross11 <jgross11@ycp.edu>
2020-12-18 20:38:58 -0500

    Create CampaignSpawnObject.cs
    Created class to store a predetermined zombie's type, spawn position, and spawn time

---
+++ Assets/scripts/Playmode Scripts/CampaignSpawnObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// container class that holds zombie spawn information
+public class CampaignSpawnObject
+{
+    // index in zombie spawner's zombie type array to spawn
+    public int zombieIndex;
+
+    // position in zombie spawner's zombie position array to spawn at
+    public int positionIndex;
+
+    // time at which to spawn this zombie
+    public float spawnTime;
+
+    public CampaignSpawnObject(int zIndex, int posIndex, float spaTime){
+        zombieIndex = zIndex;
+        positionIndex = posIndex;
+        spawnTime = spaTime;
+    }
+}
