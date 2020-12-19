6f816a4ab15c23d6fe01d6cbe32de848659f43c8
jgross11 <jgross11@ycp.edu>
2020-12-19 13:53:03 -0500

    Update CampaignLogicHandler.cs
    added available-tower checking at the start of each wave

--- Assets/scripts/Playmode Scripts/CampaignLogicHandler.cs
+++ Assets/scripts/Playmode Scripts/CampaignLogicHandler.cs
@@ -7,6 +7,9 @@ public class CampaignLogicHandler : MonoBehaviour
     // used to reveal only the appropriate towers for player to use
     public playMenuHandler menuScript;
 
+    // copy of entire tower list to use when revealing more towers
+    public GameObject[] allAvailableEmployees;
+
     // the prefab for a blank board
     public GameObject clearBoardPrefab;
 
@@ -34,7 +37,7 @@ public class CampaignLogicHandler : MonoBehaviour
     void Start(){
         // TODO determine what the scaling formula for tower unlocks in campaign mode is
         // allow only the appropriate employees to be used, more to be added as campaign progresses
-        menuScript.availableEmployees = TrimAvailableEmployees(SaveObject.maxCampaignWave / 5 + 1);
+        allAvailableEmployees = menuScript.availableEmployees;
 
         // obtain original board position to reference when placing new board upon wave ending
         initialBoardPos = boardInstance.transform.position;
@@ -83,6 +86,10 @@ public class CampaignLogicHandler : MonoBehaviour
         Destroy(boardInstance);
         boardInstance = Instantiate(clearBoardPrefab, initialBoardPos, Quaternion.identity);
 
+        // calculate and reveal appropriate towers in menu
+        menuScript.availableEmployees = TrimAvailableEmployees(SaveObject.maxCampaignWave / 5 + 1);
+        menuScript.CreateTowerPreview();
+
         // reset coin spawn timer
         coinScript.currentTime = 0;
 
@@ -106,7 +113,7 @@ public class CampaignLogicHandler : MonoBehaviour
 
         // add unlocked tower to sub array
         for(int i = 0; i < newIndexAmount; i++){
-            result[i] = menuScript.availableEmployees[i];
+            result[i] = allAvailableEmployees[i];
         }
 
         // return subarray
