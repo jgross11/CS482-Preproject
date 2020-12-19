5c73519a47233bc61bf8a3eda8d946dfccf1d424
jgross11 <jgross11@ycp.edu>
2020-12-19 10:44:14 -0500

    Create Bama.cs
    Created script for bama woman activation / movement upon colliding with zombie

---
+++ Assets/scripts/Playmode Scripts/Bama.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bama : MonoBehaviour
+{
+    
+    private bool isActive;
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isActive){
+            transform.position += Vector3.right * Time.deltaTime * movementSpeed;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll){
+        if(coll.tag == "zombie"){
+            isActive = true;
+            Destroy(coll.gameObject);
+        }
+    }
+}
