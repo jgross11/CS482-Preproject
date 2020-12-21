0c1c0fc44a1e1397985dd694342a3557e17ee5bd
andersonjackc <60368246+andersonjackc@users.noreply.github.com>
2020-12-21 12:19:02 -0500

    Required Action Script for the Pillow

---
+++ Assets/scripts/EmployeeActionScripts/PillowActionScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillowActionScript : ActionScript
+{
+
+    //this needed to be added for there not to be errors, or the way Zombie/Employee interaction would have needed to be completely rewritten
+
+    public override bool CanAct()
+    {
+        return true;
+    }
+
+    public override void Act(int attack)
+    {
+        
+    }
+}
