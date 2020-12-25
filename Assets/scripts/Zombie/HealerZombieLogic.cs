99c3778f03e656ef2c652c99771a68804965c490
jgross11 <jgross11@ycp.edu>
2020-12-24 20:44:02 -0500

    Bug fix
    Fixed lag spikes when multiple zombie healers collided by changing their rigidbody type and only toggling to the computationally expensive type (still a necessity) 3 frames every 2 seconds

--- Assets/scripts/Zombie/HealerZombieLogic.cs
+++ Assets/scripts/Zombie/HealerZombieLogic.cs
@@ -24,6 +24,12 @@ public class HealerZombieLogic : ZombieLogicScript
     // this zombie's Zombie script
     public Zombie zombieScript;
 
+    // parent rigidbody
+    public Rigidbody2D parentRGB;
+
+    // dynamic rigidbody counter
+    public int rgbyCount = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,12 +43,25 @@ public class HealerZombieLogic : ZombieLogicScript
 
         // obtain reference to this zombie
         zombieScript = transform.parent.GetComponent<Zombie>();
+
+        parentRGB = transform.parent.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if the 2 frame fully-enabled rigidbody window is over
+        if(parentRGB.bodyType == RigidbodyType2D.Dynamic && rgbyCount++ > 1){
 
+            // apply a small force (doesn't actually move the object) so other rigidbodies can detect this one
+            parentRGB.AddForce(Vector2.up);
+
+            // change this zombie's rigidbody back to static to minimize lag
+            parentRGB.bodyType = RigidbodyType2D.Static;
+
+            // reset frame window counter
+            rgbyCount = 0;
+        }
     }
 
     // this healer simply walks forward for a predetermined period of time
@@ -58,9 +77,15 @@ public class HealerZombieLogic : ZombieLogicScript
     // this healer can act after it has stopped moving, so long as either it has lost health
     // or there is one zombie for it to heal TODO fix this tower healing when no one needs healed in an efficient manner
     public override bool CanAct(){
-        return timeMoved >= movementTime && (zombiesInRange.Count != 0 || zombieScript.currentHealth != zombieScript.maxHealth);
+        return timeMoved >= movementTime;
     }
     public override void Act(){
+
+        // start 2 frame window where the zombie's rigidbody is detectable by other zombie healers
+        // small window where rigidbody is fully enabled is necessary, as otherwise, other zombie healers would never detect 
+        // this healer, even if it is in their scanning range, due to Static rigidbody <---> Static rigidbody collisions not being recorded
+        parentRGB.bodyType = RigidbodyType2D.Dynamic;
+
         // initial target is this zombie's script
         Zombie target = zombieScript;
 
@@ -69,10 +94,21 @@ public class HealerZombieLogic : ZombieLogicScript
 
             // iterate through zombies in range and determine zombie with least amount of health
             // TODO determine if healing should be prioritized by percentage or raw difference 
-            foreach(KeyValuePair<int, Zombie> entry in zombiesInRange)
+
+            // what a marvelous workaround this is
+            // no guarantee that each Zombie script in dict will exist - if zombie has died since last action, will not show up in dict
+            // however, if zombie dies during the scanning of the dict, the enumerator will throw an error due to value being removed during enumeration
+            // so, cannot iterate directly through dict. Workaround: obtain a list of the dict's keys, iterate through that, and check if Zombie value is null
+            // if so, safe to remove the key from dict, otherwise a valid Zombie reference was obtained!
+            List<int> keyList = new List<int>(zombiesInRange.Keys);
+            foreach(int key in keyList)
             {
-                if(entry.Value.currentHealth < target.currentHealth){
-                    target = entry.Value;
+                Zombie zInRangeScript = zombiesInRange[key];
+                if(zInRangeScript == null) {
+                    zombiesInRange.Remove(key);
+                }
+                else if(zInRangeScript.currentHealth < target.currentHealth){
+                    target = zInRangeScript;
                 }
             }
         }
@@ -83,8 +119,7 @@ public class HealerZombieLogic : ZombieLogicScript
 
     void OnTriggerEnter2D(Collider2D coll){
         // if a new zombie comes within range
-        if(coll.tag == "zombie"){
-
+        if(coll.tag == "zombie" && !zombiesInRange.ContainsKey(coll.gameObject.GetInstanceID())){
             // add zombie to dict for targeting purposes when actable
             zombiesInRange.Add(coll.gameObject.GetInstanceID(), coll.gameObject.GetComponent<Zombie>());
         }
@@ -93,18 +128,8 @@ public class HealerZombieLogic : ZombieLogicScript
     void OnTriggerStay2D(Collider2D coll){
         // if a new zombie comes within range
         if(coll.tag == "zombie" && !zombiesInRange.ContainsKey(coll.gameObject.GetInstanceID())){
-
             // add zombie to dict for targeting purposes when actable
             zombiesInRange.Add(coll.gameObject.GetInstanceID(), coll.gameObject.GetComponent<Zombie>());
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D coll){
-        // if a zombie leaves this zombie's range
-        if(coll.tag == "zombie"){
-
-            // remove zombie from dict
-            zombiesInRange.Remove(coll.gameObject.GetInstanceID());
         }
     }
 }
