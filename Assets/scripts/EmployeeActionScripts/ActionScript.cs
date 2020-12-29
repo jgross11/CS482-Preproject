7b2641f7fd6e26ecd6c8c5bda8c1285f33d72be7
jgross11 <jgross11@ycp.edu>
2020-12-29 13:01:59 -0500

    Update ActionScript.cs
    added obtaining of employee reference to abstract action script, as every action script will need such a reference

--- Assets/scripts/EmployeeActionScripts/ActionScript.cs
+++ Assets/scripts/EmployeeActionScripts/ActionScript.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 // abstract class representing an action taken by a tower (and zombie?)
 public abstract class ActionScript : MonoBehaviour
 {
+    // reference to this tower's employee script
+    protected Employee employeeScript;
+
+    // obtain reference here, can override via override keyword
+    // if other stuff needs to happen in start
+    // but overrided method must assign employee script
+    public virtual void Start(){
+        employeeScript = transform.parent.GetComponent<Employee>();
+    }
+    
     // must return true if the tower can act, false otherwise
     public abstract bool CanAct();
 
