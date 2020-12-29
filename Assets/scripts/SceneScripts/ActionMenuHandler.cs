0ffd17d2a020981a63e83a388395821857fc02e7
jgross11 <jgross11@ycp.edu>
2020-12-29 14:06:57 -0500

    Update ActionMenuHandler.cs
    changed unlocking of swarm mode to align with correct completion of campaign mode

--- Assets/scripts/SceneScripts/ActionMenuHandler.cs
+++ Assets/scripts/SceneScripts/ActionMenuHandler.cs
@@ -17,7 +17,7 @@ public class ActionMenuHandler : MonoBehaviour
     void Start()
     {
         // swarm button unlocked once a certain campaign wave has been reached
-        swarmButton.interactable = SaveObject.maxCampaignWave > 39;
+        swarmButton.interactable = SaveObject.maxCampaignWave > 19;
 
         // reserach button unlocked once a certain swam wave has been reached
         researchButton.interactable = SaveObject.maxSwarmWave > 20;
