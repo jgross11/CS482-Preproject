5e665bfd3f35ac412fbd20c8e645e7c969ab661a
jgross11 <jgross11@ycp.edu>
2021-01-03 09:39:47 -0500

    Created Agent M projectile
    Both script and prefab

---
+++ Assets/scripts/EmployeeActionScripts/MProjectile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MProjectile : MonoBehaviour
+{
+    // damage value 
+    public int value;
+
+    // movement speed 
+    public float moveSpeed;
+
+    // employee reference
+    private Employee employeeScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move projectile forward relative to its move speed
+        transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+        transform.Rotate(0, 0, -2);
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        
+        // if collided with zombie
+        if(col.tag == "zombie"){
+            // damage the zombie by this projectile's damage value
+            col.GetComponent<Zombie>().Damage(value);
+
+            // give experience to agent tower
+            employeeScript.AddExperience(value);
+
+            // destroy the projectile
+            Destroy(this.gameObject);
+        }
+    }
+
+    // set this projectile's damage value and tower script reference
+    public void SetValues(Employee emp, int val){
+        value = val;
+        employeeScript = emp;
+    }
+}
