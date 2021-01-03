9d4277ed6248fb529bcf0387fac0352c68a527b3
jgross11 <jgross11@ycp.edu>
2021-01-03 18:35:47 -0500

    Created swarm selection tile

---
+++ Assets/scripts/SceneScripts/SwarmSelectionTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmSelectionTile : MonoBehaviour
+{
+    
+    public SwarmMenuHandler handler;
+    public int index;
+
+    void OnMouseEnter(){
+        handler.UpdateHover(index);
+    }
+
+    void OnMouseDown(){
+        if(handler.selectionToMakeIndex != -1){
+            handler.UpdateSelectedTower(index);
+        }   
+    }
+
+    void OnMouseExit(){
+        handler.UpdateHover(-1);
+    }
+}
