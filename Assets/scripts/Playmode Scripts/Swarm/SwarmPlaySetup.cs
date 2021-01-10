e12914edf725d040ba8ce2d8d064e13ee5e3102c
jgross11 <jgross11@ycp.edu>
2021-01-10 13:42:20 -0500

    Create SwarmPlaySetup.cs
    create script to handle loading of swarm towers from menu upon entering swarm play mode

---
+++ Assets/scripts/Playmode Scripts/Swarm/SwarmPlaySetup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmPlaySetup : MonoBehaviour
+{
+    
+    public playMenuHandler menuScript;
+    public SwarmZombieSpawner spawnerScript;
+    public GameObject[] towers;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadSelectedTowers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LoadSelectedTowers(){
+        // (SaveObject.swarmTowerSelections >> i*4) & 0xf
+        for(int i = 0; i < menuScript.availableEmployees.Length; i++){
+            menuScript.availableEmployees[i] = towers[(SaveObject.swarmTowerSelections >> i*4) & 0xf];
+        }
+        menuScript.CreateTowerPreview();
+    }
+}
